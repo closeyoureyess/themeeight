7a5146939c1072822c78275dc84ff6f26cda5770
vsirik <vadimss.dremin@gmail.com>
2025-04-24 22:08:51 +0300

    fx

--- src/test/java/com/effective/themeeight/AbstractContainerTest.java
+++ src/test/java/com/effective/themeeight/AbstractContainerTest.java
@@ -29,7 +29,6 @@ public abstract class AbstractContainerTest {
         registry.add("spring.datasource.url", POSTGRES_CONTAINER::getJdbcUrl);
         registry.add("spring.datasource.username", POSTGRES_CONTAINER::getUsername);
         registry.add("spring.datasource.password", POSTGRES_CONTAINER::getPassword);
-
         registry.add("spring.data.redis.host", REDIS_CONTAINER::getHost);
         registry.add("spring.data.redis.port", () -> REDIS_CONTAINER.getMappedPort(6379));
     }
